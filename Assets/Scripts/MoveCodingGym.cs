68be24d2a40338f4b769ec3b41a9eabca1e09aeb
ferremia <ferremia@OA-S141-15>
2026-03-10 11:52:19 -0400

    interaction works!! yay

--- Assets/Scripts/MoveCodingGym.cs
+++ Assets/Scripts/MoveCodingGym.cs
@@ -6,6 +6,7 @@ public class MoveCodingGym : MonoBehaviour
 
     public Vector3 movement;
     public float speed = 5;
+    public SpriteRenderer knife;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,5 +22,15 @@ public class MoveCodingGym : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
+    }
+
+    public void Interact(InputAction.CallbackContext context)
+    {
+
+        if (knife.bounds.Contains(transform.position))
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
