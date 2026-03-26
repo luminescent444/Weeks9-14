92dbe2a358f9447a9f2ea48731ac271cef7097c3
luminescent444 <miaolf11@hotmail.com>
2026-03-26 17:37:34 -0400

    hp bar and grass interacts work

--- Assets/Scripts/HpBarAndGrass.cs
+++ Assets/Scripts/HpBarAndGrass.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HpBarAndGrass : MonoBehaviour
 {
 
     public List<GameObject> grassObjs;
     public GameObject grass;
+    public GameObject player; 
     public Vector3 spawn;
+    public float HP = 1500 ;
+    public bool poison = true;
+    public bool ateGrass = false;
+    public SpriteRenderer grassSR;
+    public SpriteRenderer playerSR;
+    public Slider HPBar;
+
+    //timer variables
+    public float timerCount;
+    public float timerLength = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,13 +30,43 @@ public class HpBarAndGrass : MonoBehaviour
         {
             spawn = new Vector3 (Random.Range(-8.5f, 9), Random.Range(-2.5f, 2.5f));
             grassObjs.Add(Instantiate(grass, spawn, transform.rotation));
-            
+            playerSR=player.GetComponent<SpriteRenderer>();
         }
+        HPBar.value = HP;
+        HPBar.maxValue = HP;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (poison==true)
+        {
+            HP = HP - 1;
+        }
+
+        if (poison == false)
+        {
+            timerCount = timerCount + Time.deltaTime;
+            if (timerCount > timerLength)
+            {
+                timerCount = 0;
+                poison = true;
+            }
+        }
+        HPBar.value = HP;
+    }
+
+    public void grassAte()
+    {
+        for(int i = 0;i < grassObjs.Count; i++)
+        {
+            grassSR = grassObjs[i].GetComponent<SpriteRenderer>(); ;
+            if (grassSR.bounds.Intersects(playerSR.bounds))
+            {
+                poison = false;
+            }
+        }
         
     }
+
 }
