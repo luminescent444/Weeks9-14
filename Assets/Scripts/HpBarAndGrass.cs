da6558cd8df47de2ac15f0348f2f74546583dbfa
luminescent444 <miaolf11@hotmail.com>
2026-03-26 16:00:57 -0400

    list works, working on positions of grass

--- Assets/Scripts/HpBarAndGrass.cs
+++ Assets/Scripts/HpBarAndGrass.cs
@@ -1,11 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class HpBarAndGrass : MonoBehaviour
 {
+
+    public List<GameObject> grassObjs;
+    public GameObject grass;
+    public Vector3 spawn;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            spawn = new Vector3 (Random.Range(-3, 3), Random.Range(-8, 8));
+            //grassObjs[i]=Instantiate(grass);
+            grassObjs.Add(Instantiate(grass,transform.position));
+            
+        }
     }
 
     // Update is called once per frame
